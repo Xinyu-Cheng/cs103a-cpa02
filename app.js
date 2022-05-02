5059456bb7337501deb51f8a28f51f5f2223d0c4
Katherine Cheng <xinyucheng@brandeis.edu>
2022-05-02 00:37:47 -0400

    fix the heroku problem

--- app.js
+++ app.js
@@ -2,25 +2,24 @@
   app.js -- This creates an Express webserver with login/register/logout authentication
 */
 
+
 // *********************************************************** //
 //  Loading packages to support the server
 // *********************************************************** //
 // First we load in all of the packages we need for the server...
-const createError = require('http-errors'); // to handle the server errors
-const express = require('express');
-const path = require('path'); // to refer to local paths
-const cookieParser = require('cookie-parser'); // to handle cookies
-const session = require('express-session'); // to handle sessions using cookies
-const debug = require('debug')('personalapp:server');
-const layouts = require('express-ejs-layouts');
-const axios = require('axios');
+const createError = require("http-errors"); // to handle the server errors
+const express = require("express");
+const path = require("path");  // to refer to local paths
+const cookieParser = require("cookie-parser"); // to handle cookies
+const session = require("express-session"); // to handle sessions using cookies
+const debug = require("debug")("personalapp:server"); 
+const layouts = require("express-ejs-layouts");
+const axios = require("axios")
+var MongoDBStore = require('connect-mongodb-session')(session);
 
 // *********************************************************** //
 //  Loading models
 // *********************************************************** //
-const ToDoItem = require('./models/ToDoItem');
-const Course = require('./models/Course');
-const Schedule = require('./models/Schedule');
 const SchoolList = require('./models/SchoolList');
 const College = require('./models/College');
 
@@ -28,60 +27,80 @@ const College = require('./models/College');
 // *********************************************************** //
 //  Loading JSON datasets
 // *********************************************************** //
-const courses = require('./public/data/courses20-21.json');
 const colleges = require('./public/data/colleges.json');
 
 // *********************************************************** //
 //  Connecting to the database
 // *********************************************************** //
 
-const mongoose = require('mongoose');
+const mongoose = require( 'mongoose' );
+
 const mongodb_URI = process.env.mongodb_URI
 
-mongoose.connect(mongodb_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect( mongodb_URI, { useNewUrlParser: true, useUnifiedTopology: true } );
 // fix deprecation warnings
-mongoose.set('useFindAndModify', false);
+mongoose.set('useFindAndModify', false); 
 mongoose.set('useCreateIndex', true);
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function () {
-  console.log('we are connected!!!');
-});
+db.once('open', function() {console.log("we are connected!!!")});
 
 // *********************************************************** //
-// Initializing the Express server
+// Initializing the Express server 
 // This code is run once when the app is started and it creates
 // a server that respond to requests by sending responses
 // *********************************************************** //
 const app = express();
 
-// Here we specify that we will be using EJS as our view engine
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+var store = new MongoDBStore({
+  uri: mongodb_URI,
+  collection: 'mySessions'
+});
 
-// this allows us to use page layout for the views
+// Catch errors
+store.on('error', function(error) {
+  console.log(error);
+});
+
+app.use(require('express-session')({
+  secret: 'This is a secret',
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24 * 7 // 1 week
+  },
+  store: store,
+  // Boilerplate options, see:
+  // * https://www.npmjs.com/package/express-session#resave
+  // * https://www.npmjs.com/package/express-session#saveuninitialized
+  resave: true,
+  saveUninitialized: true
+}));
+
+// Here we specify that we will be using EJS as our view engine
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+
+
+// this allows us to use page layout for the views 
 // so we don't have to repeat the headers and footers on every page ...
 // the layout is in views/layout.ejs
 app.use(layouts);
 
 // Here we process the requests so they are easy to handle
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 // Here we specify that static files will be in the public folder
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // Here we enable session handling using cookies
 app.use(
   session({
-    secret: 'zzbbyanana789sdfa8f9ds8f90ds87f8d9s789fds', // this ought to be hidden in process.env.SECRET
+    secret: "zzzbbyanana789sdfa8f9ds8f90ds87f8d9s789fds", // this ought to be hidden in process.env.SECRET
     resave: false,
-    saveUninitialized: false,
+    saveUninitialized: false
   })
 );
 
@@ -254,57 +273,62 @@ app.get(
   }
 );
 
+
 // here we catch 404 errors and forward to error handler
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
   next(createError(404));
 });
 
 // this processes any errors generated by the previous routes
 // notice that the function has four parameters which is how Express indicates it is an error handler
-app.use(function (err, req, res, next) {
+app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
+
 
 // *********************************************************** //
 //  Starting up the server!
 // *********************************************************** //
 //Here we set the port to use between 1024 and 65535  (2^16-1)
-const port = process.env.PORT || "5000";
+const port = process.env.PORT || "5000"; 
 console.log('connecting on port '+port)
-app.set('port', port);
+
+app.set("port", port);
 
 // and now we startup the server listening on that port
-const http = require('http');
+const http = require("http");
+const { reset } = require("nodemon");
+const { pseudoRandomBytes } = require("crypto");
 const server = http.createServer(app);
 
 server.listen(port);
 
 function onListening() {
   var addr = server.address();
-  var bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port;
-  debug('Listening on ' + bind);
+  var bind = typeof addr === "string" ? "pipe " + addr : "port " + addr.port;
+  debug("Listening on " + bind);
 }
 
 function onError(error) {
-  if (error.syscall !== 'listen') {
+  if (error.syscall !== "listen") {
     throw error;
   }
 
-  var bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port;
+  var bind = typeof port === "string" ? "Pipe " + port : "Port " + port;
 
   // handle specific listen errors with friendly messages
   switch (error.code) {
-    case 'EACCES':
-      console.error(bind + ' requires elevated privileges');
+    case "EACCES":
+      console.error(bind + " requires elevated privileges");
       process.exit(1);
       break;
-    case 'EADDRINUSE':
-      console.error(bind + ' is already in use');
+    case "EADDRINUSE":
+      console.error(bind + " is already in use");
       process.exit(1);
       break;
     default:
@@ -312,8 +336,8 @@ function onError(error) {
   }
 }
 
-server.on('error', onError);
+server.on("error", onError);
 
-server.on('listening', onListening);
+server.on("listening", onListening);
 
 module.exports = app;
