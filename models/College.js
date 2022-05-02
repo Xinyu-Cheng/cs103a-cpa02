2f8df6aabb1fcf27627f04d86ea32a930759e7e4
Katherine Cheng <xinyucheng@brandeis.edu>
2022-05-01 22:23:54 -0400

    deploy on heroku

--- models/College.js
+++ models/College.js
@@ -6,10 +6,10 @@ const Mixed = Schema.Types.Mixed;
 
 var collegeSchema = Schema( {
     unitID: Number,
-    name: String, //Institution Name
-    state: String, //State abbreviation (HD2020)
-    websiteAddress: String, //Institution's internet website address (HD2020)
-    city: String, //City location of institution (HD2020)
+    name: String, 
+    state: String,
+    websiteAddress: String, 
+    city: String,
 } );
 
 module.exports = mongoose.model( 'College', collegeSchema );
